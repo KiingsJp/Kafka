d39f9c187a065de048209e858468c0073676b42c
Kings <systemskings@gmail.com>
2025-03-30 15:44:33 -0300

    adicionado varios consumidores e produtores

---
+++ src/main/java/br/com/kafka/ecommerce/LogService.java
@@ -0,0 +1,55 @@
+package br.com.kafka.ecommerce;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+
+import java.time.Duration;
+import java.util.Collections;
+import java.util.Properties;
+import java.util.regex.Pattern;
+
+public class LogService {
+    public static void main(String[] args) {
+
+        try(var consumer = new KafkaConsumer<String, String>(properties())) {
+            consumer.subscribe(Pattern.compile("ECOMMERCE.*"));
+            while (true) {
+                try {
+                    var records = consumer.poll(Duration.ofMillis(100));
+                    if(!records.isEmpty()) {
+                        System.out.println("Records found: " + records.count());
+
+                        records.forEach(record -> {
+                            System.out.println("------------------------------");
+                            System.out.println("LOG data:");
+                            System.out.println("Topic: " + record.topic());
+                            System.out.println("Key: " + record.key());
+                            System.out.println("Value: " + record.value());
+                            System.out.println("OffSet: " + record.offset());
+                            System.out.println("Partition: " + record.partition());
+
+                            try {
+                                Thread.sleep(5000);
+                            } catch (InterruptedException e) {
+                                System.out.println("Interrupted while waiting for record");
+                            }
+                        });
+                    }
+                } catch (Exception e) {
+                    System.out.println("Error while consuming records: " + e.getMessage());
+                    break;
+                }
+            }
+        }
+    }
+
+    public static Properties properties() {
+        var properties = new Properties();
+        properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+        properties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.put(ConsumerConfig.GROUP_ID_CONFIG, LogService.class.getSimpleName());
+        return properties;
+    }
+}
--- src/main/java/br/com/kafka/ecommerce/NewOrderMain.java
+++ src/main/java/br/com/kafka/ecommerce/NewOrderMain.java
@@ -1,5 +1,6 @@
 package br.com.kafka.ecommerce;
 
+import org.apache.kafka.clients.producer.Callback;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
@@ -11,15 +12,23 @@ public class NewOrderMain {
 
     public static void main(String[] args) {
         try (var producer = new KafkaProducer<String, String>(properties())) {
-            var value = "9123, 8123, 7123";
-            var record = new ProducerRecord<>("ECOMMERCE_NEW_ORDER", value, value);
-            producer.send(record, (data, exception) -> {
+
+            Callback callback = (data, exception) -> {
                 if(exception != null) {
                     System.out.println("Erro: " + exception.getMessage());
                     return;
                 }
                 System.out.println("sucesso enviado - topico: " + data.topic() + " offset: " + data.offset() + " partition: " + data.partition() + " timestamp: " + data.timestamp() );
-            });
+            };
+
+            var value = "9123, 8123, 7123";
+            var record = new ProducerRecord<>("ECOMMERCE_NEW_ORDER", value, value);
+            producer.send(record, callback);
+
+            var email = "kingsjp1207@gmail.com";
+            var recordEmail = new ProducerRecord<>("ECOMMERCE_SEND_EMAIL", email, email);
+            producer.send(recordEmail, callback);
+
         }
     }
 
